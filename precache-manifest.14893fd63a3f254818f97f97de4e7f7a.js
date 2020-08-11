8f88f010680dbb0c4bbb9d796b8125e795c152f3
Makankan <matadorsmca@gmail.com>
2020-08-11 12:05:48 +0530

    Updates

---
+++ precache-manifest.14893fd63a3f254818f97f97de4e7f7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9bbc596301d2bd781f53b7820f15fa0",
+    "url": "/monster-rollax/index.html"
+  },
+  {
+    "revision": "dea1955f0fcdefe8176c",
+    "url": "/monster-rollax/static/css/main.ff753939.chunk.css"
+  },
+  {
+    "revision": "7b86a2e945f28fbe074e",
+    "url": "/monster-rollax/static/js/2.854bbed5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rollax/static/js/2.854bbed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea1955f0fcdefe8176c",
+    "url": "/monster-rollax/static/js/main.f64c0900.chunk.js"
+  },
+  {
+    "revision": "bc38be5fb84cc7bd1a05",
+    "url": "/monster-rollax/static/js/runtime-main.00dbc82a.js"
+  }
+]);
